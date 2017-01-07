af41f0dad1b96475a3e26d56c2ea0a6d8e53f701
adrianbj <adrianbjones@gmail.com>
2017-01-06 22:49:27 -0800

    Split core and site actions into separate tabs. Improve handling of new actions.

---
+++ ProcessAdminActions.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    // remove scripts, because they've already been executed since we are manipulating the DOM below (WireTabs)
+    // which would cause any scripts to get executed twice
+
+    $t = $("#ActionsList");
+    //$t.find("script").remove();
+
+    $t.WireTabs({
+        items: $(".Inputfields li.WireTab"),
+        rememberTabs: true
+    });
+
+});
