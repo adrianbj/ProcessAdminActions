d95cafe225c958db38e015ebe5df329ed9e826db
adrianbj <adrianbjones@gmail.com>
2018-09-30 19:54:03 +1000

    Fix layout bug in default PW admin theme.

--- ProcessAdminActions.js
+++ ProcessAdminActions.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     // remove scripts, because they've already been executed since we are manipulating the DOM below (WireTabs)
     // which would cause any scripts to get executed twice
 
-    $t = $("#ActionsList");
+    $t = $("#AdminActionsList");
     //$t.find("script").remove();
 
     $t.WireTabs({
